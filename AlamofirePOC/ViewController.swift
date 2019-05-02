9c53b238c5f298c46f414fa1ea6014baa55bc150
Ernesto Gonzalez <ernesto.gonzalez@gorillalogic.com>
2019-05-01 18:10:54 -0600

    Initial Commit

---
+++ AlamofirePOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofirePOC
+//
+//  Created by Ernesto Gonzalez on 5/1/19.
+//  Copyright © 2019 Ernesto Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
